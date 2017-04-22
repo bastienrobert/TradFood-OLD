e6cdb8c5b5171fed5446f0e8dfdf966e1c3df090
Bastien Robert <bastienrbt@protonmail.com>
2017-04-22 16:33:19 +0200

    Remove uggly global vars, now beautifulized in views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,6 @@
 class PagesController < ApplicationController
    def home
      @cities = City.all
-     @citiesStared = City.where(status:2).limit(5)
      @recipes = Recipe.all
-     @recipesStared = Recipe.where(status:2).limit(5)
-     @recipesLast = Recipe.last(3).reverse
    end
  end
